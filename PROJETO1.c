dee623e51ef3ece076b8074eaad371a881971dfb
Luís Otavio <104238414+CodeLuiss@users.noreply.github.com>
2024-07-15 10:54:24 -0300

    Opção de saida
    Incluso no sistema a opção de saida para o usuario

--- PROJETO1.c
+++ PROJETO1.c
@@ -146,9 +146,9 @@ int main()
 	    	break;
 	    	
 	    	case 4:
-	    		printf("Obrigado por utilizar o sistema!\n");
-	    		return 0;
-	    		break;
+	    	printf("Obrigado por utilizar o sistema!\n");
+	    	return 0;
+	    	break;
 	    	
 		
 	    	default:
