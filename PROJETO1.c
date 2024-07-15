409bf71fcb08094c0c5267d470d97ff91fe58410
Luís Otavio <104238414+CodeLuiss@users.noreply.github.com>
2024-07-15 10:52:34 -0300

    Opção de saida
    Incluso no sistema a opção de saida para o usuario

--- PROJETO1.c
+++ PROJETO1.c
@@ -123,7 +123,8 @@ int main()
 	    printf("Escolha a opção desejada do menu:\n\n");
    	    printf("\t1 - Registar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
-	    printf("\t3 - Deletar nomes\n");
+	    printf("\t3 - Deletar nomes\n\n");
+	    printf("\t4 - Sair do sistema\n\n");
 	    printf("Opção: "); 	//fim do menu
  
 	    scanf("%d", &opcao); //armazenando escolhas do usuario
@@ -143,6 +144,11 @@ int main()
 	    	case 3:
 	    	deletar();//chamada de funções
 	    	break;
+	    	
+	    	case 4:
+	    		printf("Obrigado por utilizar o sistema!\n");
+	    		return 0;
+	    		break;
 	    	
 		
 	    	default:
